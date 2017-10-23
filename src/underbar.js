850596af68ddb0840669b62cc2a48a1a2a9f42b5
James Jelenko <jamesjelenko@gmail.com>
2017-10-23 12:34:21 -0700

    implemented map function

--- src/underbar.js
+++ src/underbar.js
@@ -108,14 +108,17 @@
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array, isSorted, iterator) {
+
   };
 
 
   // Return the results of applying an iterator to each element.
   _.map = function(collection, iterator) {
-    // map() is a useful primitive iteration function that works a lot
-    // like each(), but in addition to running the operation on all
-    // the members, it also maintains an array of results.
+    var results = [];
+    for (var idx = 0; idx <= collection.length - 1; idx++){
+      results.push(iterator(collection[idx]))
+    }
+    return results
   };
 
   /*
